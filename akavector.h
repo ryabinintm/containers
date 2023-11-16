077c94ef97282a6187d7f65375a0ed7b397bbe63
Alexander Ryabinin <ryabinintm@yandex.ru>
2023-11-16 17:17:20 +0300

    Ready akavector interface

--- akavector.h
+++ akavector.h
@@ -1,25 +1,21 @@
 #pragma once
-//#include <cstddef>
 
-//namespace Container {
-  template<typename T>
-  class Akavector {
-  public:
-   Akavector();
-   ~Akavector();
-   void push_back(T value);
-   void insert(int pos, T value);
-   void erase(int element);
-   int size();
-   T operator[](int index);
-   void info();
-   void print();
-  private:
-    int m_size;
-    int capacity;
-    T *values;
-    void check_capacity();
-  };
-  #include "akavector.cpp"
-//}
-
+template<typename T>
+class Akavector {
+public:
+ Akavector();
+ ~Akavector();
+ void push_back(T value);
+ void insert(int pos, T value);
+ void erase(int element);
+ int size();
+ T operator[](int index);
+ void info();
+ void print();
+private:
+  int m_size;
+  int capacity;
+  T *values;
+  void check_capacity();
+};
+#include "akavector.cpp"
--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include "akavector.h"
-/*
+
 namespace {
   template<typename T>
-  void print_values(const T values) {
+  void print_values(T &vals) {
     bool is_common_first = false;
-    for (int i = 0; i < values.size(); i++) {
+    for (int i = 0; i < vals.size(); i++) {
       if (is_common_first) {
         std::cout << ", ";
       }
-      std::cout << values[i];
+      std::cout << vals[i];
       is_common_first = true;
     }
     std::cout << std::endl;
   }
 }
-*/
 
 
 int main () {
@@ -28,7 +27,7 @@ int main () {
 
   // Displaying the contents of the container on the screen
   // Expected result: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-  myvector.print();
+  print_values(myvector);
 
   // Displaying the container size on the screen
   // Expected result: 10
@@ -41,14 +40,14 @@ int main () {
 
   // Displaying the contents of the container on the screen
   // Expected result: 0, 1, 3, 5, 7, 8, 9
-  myvector.print();
+  print_values(myvector);
 
   // Adding element 10 to the beginning of the container
   myvector.insert(0, 10);
 
   // Displaying the contents of the container on the screen
   // Expected result: 10, 0, 1, 3, 5, 7, 8, 9
-  myvector.print();
+  print_values(myvector);
 
   // Adding an element to the middle of the container
   // Expected result: 10, 0, 1, 3, 20, 5, 7, 8, 9
@@ -57,14 +56,14 @@ int main () {
 
   // Displaying the contents of the container on the screen
   // Expected result: 10, 0, 1, 3, 20, 5, 7, 8, 9
-  myvector.print();
+  print_values(myvector);
 
   // Adding element 30 to the end of the container
   myvector.push_back(30);
 
   // Displaying the contents of the container on the screen
   // Expected result: 10, 0, 1, 3, 20, 5, 7, 8, 9, 30
-  myvector.print();
+  print_values(myvector);
 
   return 0;
 }
